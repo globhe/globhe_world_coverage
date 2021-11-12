9e0af0e132b9e59630ab16d5cf81e9d0eac7229d
Rémy Ghalayini <63195370+rghalayini@users.noreply.github.com>
2021-11-12 14:36:57 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,17 @@
 const data = [
+    { pilots: "1", ISO3: "BEN", country:"Benin" },
     { pilots: "1", ISO3: "CHN", country:"China" },
-    { pilots: "1", ISO3: "DOM", country:"Dominican Republic" },
-    { pilots: "1", ISO3: "FRA", country:"France" },
-    { pilots: "1", ISO3: "IND", country:"India" },
-    { pilots: "1", ISO3: "IDN", country:"Indonesia" },
     { pilots: "1", ISO3: "LSO", country:"Lesotho" }, 
-    { pilots: "1", ISO3: "MDV", country:"Maldives" },
+    { pilots: "1", ISO3: "MWI", country:"Malawi" },
     { pilots: "1", ISO3: "MLI", country:"Mali" }, 
-    { pilots: "1", ISO3: "NGA", country:"Nigeria" },
-    { pilots: "1", ISO3: "ESP", country:"Spain" },
+    { pilots: "1", ISO3: "NOR", country:"Norway" },    
+    { pilots: "1", ISO3: "SEN", country:"Senegal" },
+    { pilots: "1", ISO3: "ZAF", country:"South Africa" },
     { pilots: "1", ISO3: "SWE", country:"Sweden" }, 
     { pilots: "1", ISO3: "TGO", country:"Togo" },
     { pilots: "1", ISO3: "ARE", country:"UAE" },
     { pilots: "1", ISO3: "GBR", country:"United Kingdom" },
     { pilots: "1", ISO3: "USA", country:"United States" },
-    { pilots: "1", ISO3: "ZMB", country:"Zambia" },
-    { pilots: "1", ISO3: "ZWE", country:"Zimbabwe" },
   ];
 
   export default data;
