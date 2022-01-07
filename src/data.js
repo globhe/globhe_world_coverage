f28f33f02f9288f7614e38038ab0b3a898d880d3
Rémy Ghalayini <63195370+rghalayini@users.noreply.github.com>
2022-01-07 15:30:58 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,8 +1,9 @@
-const data = [
-    { pilots: "1", ISO3: "CHN", country:"China" },  
+cconst data = [
+    { pilots: "1", ISO3: "MEX", country:"Mexico" },  
     { pilots: "1", ISO3: "SWE", country:"Sweden" }, 
     { pilots: "1", ISO3: "TGO", country:"Switzerland" },   
     { pilots: "1", ISO3: "ARE", country:"UAE" },   
+    { pilots: "1", ISO3: "GBR", country:"United Kingdom" },
     { pilots: "1", ISO3: "USA", country:"United States" },
   ];
 
