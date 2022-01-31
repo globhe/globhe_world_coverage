31fb12d65cf032be7c64e2c209855a0656f0b46a
Rémy Ghalayini <63195370+rghalayini@users.noreply.github.com>
2022-01-31 13:47:49 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,16 +1,5 @@
 const data = [
-    { pilots: "1", ISO3: "AUT", country:"Austria" },
-    { pilots: "1", ISO3: "BRA", country:"Brazil" },
-    { pilots: "1", ISO3: "GRC", country:"Greece" },
-    { pilots: "1", ISO3: "IRL", country:"Ireland" },
-    { pilots: "1", ISO3: "KEN", country:"Kenya" },
-    { pilots: "1", ISO3: "LSO", country:"Lesotho" }, 
-    { pilots: "1", ISO3: "PER", country:"Peru" },   
-    { pilots: "1", ISO3: "SEN", country:"Senegal" }, 
     { pilots: "1", ISO3: "SWE", country:"Sweden" },   
-    { pilots: "1", ISO3: "CHE", country:"Switzerland" },   
-    { pilots: "1", ISO3: "ARE", country:"UAE" },   
-    { pilots: "1", ISO3: "GBR", country:"United Kingdom" },
     { pilots: "1", ISO3: "USA", country:"United States" },
   ];
 
